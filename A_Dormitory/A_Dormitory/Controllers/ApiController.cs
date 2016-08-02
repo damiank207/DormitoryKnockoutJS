dac55333fdc77ce1ac11d0a1af5943d53dc624ac
bartosz.szewczyk <bartosz.szewczyk@DESKTOP-3J564LR>
2016-08-02 14:26:30 +0200

    hjhjh

---
+++ A_Dormitory/A_Dormitory/Controllers/ApiController.cs
@@ -0,0 +1,20 @@
+﻿using A_Dormitory.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace A_Dormitory.Controllers
+{
+    public class ApiController : Controller
+    {
+        // GET: Api
+        private DormitoryContext db = new DormitoryContext();
+
+        public JsonResult Citizenship()
+        {
+            return Json(db.CitizenshipDict);
+        }
+    }
+}
